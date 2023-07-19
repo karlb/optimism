fa346b767a335ba8fe506b107ae7ed572c316811
Karl Bartel <karl.bartel@clabs.co>
2023-07-19 17:39:03 +0200

    Add Registry contract
    I updated the solidity version pragma with
    ```
    sed -i '' 's/0\.5\.13/0.8.0/' **.sol
    ```
    to match the version of the already included openzepplin dependencies.

---
+++ packages/contracts-bedrock/contracts/celo/common/Initializable.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.0;
+
+contract Initializable {
+  bool public initialized;
+
+  constructor(bool testingDeployment) public {
+    if (!testingDeployment) {
+      initialized = true;
+    }
+  }
+
+  modifier initializer() {
+    require(!initialized, "contract already initialized");
+    initialized = true;
+    _;
+  }
+}
